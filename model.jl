c4c8198ac002a982d239d3fad998384f2ac7f8b1
Anais DM <anaiis.dcm@gmail.com>
2026-02-12 20:31:18 +0100

    Add all families of assets + storage constraints but infeasible problem when tested (Oriane)

--- model.jl
+++ model.jl
@@ -6,21 +6,23 @@ using HiGHS
 using XLSX
 
 Tmax = 168 #optimization for 1 week (7*24=168 hours)
+duration_t = 1
 
-#data for load and inter generation
-data_file = "inputs.xlsx"
+#Data loading
+file = "inputs.xlsx"
+sheet = "Données instantanées"
 
 #data for load generation
-load = XLSX.readdata(data_file, "Données instantanées", "C2:C169")
-load = Float64.(vec(load))
+load = XLSX.readdata(file, sheet, "C2:C169")
+load = Float64.(coalesce(vec(load),0.0))
 
 #data for inter generation
-solar = XLSX.readdata(data_file, "Données instantanées", "E2:E169")
-wind_on = XLSX.readdata(data_file, "Données instantanées", "G2:G169")
-wind_off = XLSX.readdata(data_file, "Données instantanées", "H2:H169")
-hydroFO_fatal = XLSX.readdata(data_file, "Données instantanées", "K2:K169")
-hydroLake_fatal = XLSX.readdata(data_file, "Données instantanées", "L2:L169")
-thermal_fatal = XLSX.readdata(data_file, "Données instantanées", "N2:N169")
+solar = XLSX.readdata(file, sheet, "E2:E169")
+wind_on = XLSX.readdata(file, sheet, "G2:G169")
+wind_off = XLSX.readdata(file, sheet, "H2:H169")
+hydroFO_fatal = XLSX.readdata(file, sheet, "K2:K169")
+hydroLake_fatal = XLSX.readdata(file, sheet, "L2:L169")
+thermal_fatal = XLSX.readdata(file, sheet, "N2:N169")
 
 #To get rid of potential missing values
 solar           = Float64.(coalesce.(vec(solar), 0.0))
@@ -30,150 +32,215 @@ hydroLake_fatal = Float64.(coalesce.(vec(hydroLake_fatal), 0.0))
 hydroFO_fatal   = Float64.(coalesce.(vec(hydroFO_fatal), 0.0))
 thermal_fatal   = Float64.(coalesce.(vec(thermal_fatal), 0.0))
 
-#data for disp clusters
-N_disp = 39
-names_disp = XLSX.readdata(data_file, "Production électrique", "A2:A40")
-dictdisp = Dict(i=> names_disp[i] for i in 1:N_disp)
-Pout_max_disp = XLSX.readdata(data_file, "Production électrique", "B2:B40") #MW
-Pout_min_disp = XLSX.readdata(data_file, "Production électrique", "C2:C40") #MW
-#Pin_max_disp = XLSX.readdata(data_file, "Production électrique", "") #MW
-#Pin_min_disp = XLSX.readdata(data_file, "Production électrique", "") #MW
-dmin_disp = XLSX.readdata(data_file, "Production électrique", "D2:D40") #hours
-costs_disp = XLSX.readdata(data_file, "Production électrique", "E2:E40") #$/MWh
-efficacity_disp = XLSX.readdata(data_file, "Production électrique", "G2:G40")
+#data for production generation
+sheet1 = "Production électrique"
 
-Pout_max_disp = Float64.(vec(Pout_max_disp))
-Pout_min_disp = Float64.(vec(Pout_min_disp))
-dmin_disp     = Int.(vec(dmin_disp))
-costs_disp = Float64.(vec(costs_disp))
-efficacity_disp = Float64.(vec(efficacity_disp))
+names = XLSX.readdata(file, sheet1, "A2:A55")
+Pout_max = Float64.(vec(coalesce(XLSX.readdata(file, sheet1, "B2:B55"), 0.0)))
+Pout_min = Float64.(vec(coalesce(XLSX.readdata(file, sheet1, "C2:C55"), 0.0)))
+dmin = Int.(vec(coalesce.(XLSX.readdata(file, sheet1, "D2:D55"), 0)))
+Cost = Float64.(vec(coalesce.(XLSX.readdata(file, sheet1, "E2:E55"), 0.0)))
+on_init = Int.(vec(coalesce.(XLSX.readdata(file, sheet1, "F2:F55"), 0)))
+eff = Float64.(vec(coalesce.(XLSX.readdata(file, sheet1, "G2:G55"), 1.0)))
+vector = String.(vec(XLSX.readdata(file, sheet1, "J2:J55")))
+family = String.(vec(XLSX.readdata(file, sheet1, "K2:K55")))
 
+#Building a set of assets
+ASSETS = 1:length(names)
 
-#data for conv clusters
-N_conv = 31
-#Créer une feuille Excel avec les converters ?
+#Families of assets
+elec_in  = Set{Int}()
+elec_out = Set{Int}()
+gas_in   = Set{Int}()
+gas_out  = Set{Int}()
+h2_in    = Set{Int}()
+h2_out   = Set{Int}()
 
+#If an asset is in several families
+for a in ASSETS
+    categories = split(vector[a], ";")  
+    categories = strip.(categories)          
+    if "elec_in" in categories
+        push!(elec_in, a)
+    end
+    if "elec_out" in categories
+        push!(elec_out, a)
+    end
+    if "gas_in" in categories
+        push!(gas_in, a)
+    end
+    if "gas_out" in categories
+        push!(gas_out, a)
+    end
+    if "h2_in" in categories
+        push!(h2_in, a)
+    end
+    if "h2_out" in categories
+        push!(h2_out, a)
+    end
+end
 
-#data for inter assets
-N_inter = 7
-Pmax_inter = XLSX.readdata(data_file, "Production électrique", "B41:B47") #MW
-Pmax_inter = Float64.(vec(Pmax_inter))
+#Conversion assets
+conv = Set(a for a in ASSETS if eff[a] < 1.0)
 
-#costs
-c_disp = repeat(costs_disp', Tmax) #cost of disp generation $/MWh
-cuns = 5000*ones(Tmax) #cost of unsupplied energy $/MWh
-cexc = zeros(Tmax) #cost of in excess energy $/MWh
+conv_ge  = Set(a for a in conv if a ∈ gas_in && a ∈ elec_out)
+conv_eh2 = Set(a for a in conv if a ∈ elec_in && a ∈ h2_out)
+conv_gh2 = Set(a for a in conv if a ∈ gas_in && a ∈ h2_out)
+
+#Assets Functioning
+disp  = Set(a for a in ASSETS if family[a] == "disp")
+inter = Set(a for a in ASSETS if family[a] == "inter")
+solar_assets = Set(a for a in inter if names[a] == "PV1")
+wind_on_assets = Set(a for a in inter if names[a] == "Eolien terrestre 1")
+wind_off_assets = Set(a for a in inter if names[a] == "Eolien offshore 1")
+hydroFO_assets = Set(a for a in inter if names[a] == "Hydro (fil de l'eau) 1")
+hydroLake_assets = Set(a for a in inter if names[a] == "Hydro lac 1")
+thermal_fatal_assets = Set(a for a in inter if names[a] == "Déchet 1" || names[a] == "Biomasse 1")
+stock = Set(a for a in ASSETS if family[a] == "stock")
+
+#assets availability (100% du temps pour le moment)
+Avail = Dict{Int, Vector{Float64}}()
+for a in ASSETS
+    Avail[a] = ones(Tmax)
+end
 
 #############################
 #create the optimization model
 #############################
-model = Model(HiGHS.Optimizer) #J'ai laissé ce solver pour l'instant.
+model = Model(HiGHS.Optimizer)
 
 #############################
 #define the variables
 #############################
-#thermal generation variables
-#@variable(model, Pin_disp[1:Tmax,1:N_disp] >= 0)
-@variable(model, Pout_disp[1:Tmax,1:N_disp] >= 0)
-@variable(model, Pinter[1:Tmax,1:N_inter] >= 0)
-@variable(model, UCdisp[1:Tmax,1:N_disp], Bin)
-@variable(model, UPdisp[1:Tmax,1:N_disp], Bin)
-@variable(model, DOdisp[1:Tmax,1:N_disp], Bin)
 
-#unsupplied energy variables
-@variable(model, Puns[1:Tmax] >= 0)
-#in excess energy variables
-@variable(model, Pexc[1:Tmax] >= 0)
-#
-# #############################
-#define the objective function
-#############################
-@objective(model, Min, sum(Pout_disp .* c_disp) + (Puns'cuns) + (Pexc'cexc))
+#Power of functioning
+@variable(model, P_in[a in elec_in ∪ gas_in, t in 1:Tmax] ≥ 0)
+@variable(model, P_out[a in elec_out ∪ gas_out, t in 1:Tmax] ≥ 0)
+
+#Flags disp
+@variable(model, on[a in disp, t in 1:Tmax], Bin)
+@variable(model, up[a in disp, t in 1:Tmax], Bin)
+@variable(model, down[a in disp, t in 1:Tmax], Bin)
+
+#Available power
+@variable(model, P_in_max_avail[a in elec_in ∪ gas_in, t in 1:Tmax] ≥ 0)
+@variable(model, P_out_max_avail[a in elec_out ∪ gas_out, t in 1:Tmax] ≥ 0)
+
+# #stock variables
+@variable(model, E[a in stock, t in 1:Tmax] ≥ 0)
+@variable(model, isCharging[a in stock, t in 1:Tmax], Bin)
 
 #############################
 #define the constraints
 #############################
-#balance constraint
-@constraint(model, balance[t in 1:Tmax], sum(Pout_disp[t,g] for g in 1:N_disp) + solar[t] + wind_on[t] + wind_off[t] + thermal_fatal[t] + hydroFO_fatal[t] + hydroLake_fatal[t] + Puns[t] - load[t] - Pexc[t] == 0)
-# inter assets Pmax constraints
-@constraint(model, max_inter[t in 1:Tmax, g in 1:N_inter], Pinter[t,g] <= Pmax_inter[g])
-#disp unit Pmax in and out constraints
-#@constraint(model, P_max_in_disp[t in 1:Tmax, g in 1:N_disp], Pin_disp[t,g] <= Pin_max_disp[g]*UCdisp[t,g])
-@constraint(model, P_max_out_disp[t in 1:Tmax, g in 1:N_disp], Pout_disp[t,g] <= Pout_max_disp[g]*UCdisp[t,g])
-#disp unit Pmin in and out constraints
-#@constraint(model, P_min_in_disp[t in 1:Tmax, g in 1:N_disp], Pin_min_disp[g] * UCdisp[t,g] <= Pin_disp[t,g])
-@constraint(model, P_min_out_disp[t in 1:Tmax, g in 1:N_disp], Pout_min_disp[g] * UCdisp[t,g] <= Pout_disp[t,g])
+#objective function
+@objective(model, Min, sum(P_out[a,t] * Cost[a] for a in disp, t in 1:Tmax))
 
-#Initial state data
-on_init = XLSX.readdata(data_file, "Production électrique", "H2:H40")
-on_init = Int.(vec(coalesce.(on_init, 0)))
+#Inter prod
+@constraint(model, [a in solar_assets, t in 1:Tmax], P_out[a,t] == solar[t])
+@constraint(model, [a in wind_on_assets, t in 1:Tmax], P_out[a,t] == wind_on[t])
+@constraint(model, [a in wind_off_assets, t in 1:Tmax], P_out[a,t] == wind_off[t])
+@constraint(model, [a in hydroFO_assets, t in 1:Tmax], P_out[a,t] == hydroFO_fatal[t])
+@constraint(model, [a in hydroLake_assets, t in 1:Tmax], P_out[a,t] == hydroLake_fatal[t])
+@constraint(model, [a in thermal_fatal_assets, t in 1:Tmax], P_out[a,t] == thermal_fatal[t])
 
-#disp unit Dmin constraints
-for g in 1:N_disp
-        if (dmin_disp[g] > 1)
-            @constraint(model, [t in 2:Tmax], UCdisp[t,g]-UCdisp[t-1,g]==UPdisp[t,g]-DOdisp[t,g],  base_name = "fct_disp_$g")
-            @constraint(model, [t in 1:Tmax], UPdisp[t,g]+DOdisp[t,g]<=1,  base_name = "UPDOdisp_$g")
-            # Up-init constraint
-            if on_init[g] == 0
-                @constraint(model, UPdisp[1,g] == UCdisp[1,g], base_name = "iniUPdisp_$g")
-            else
-                @constraint(model, UPdisp[1,g] == 0, base_name = "iniUPdisp_$g")
-            end
-            # Down-init constraint
-            if on_init[g] > 0
-                @constraint(model, DOdisp[1,g] == 1 - UCdisp[1,g], base_name = "iniDOdisp_$g")
-            else
-                @constraint(model, DOdisp[1,g] == 0, base_name = "iniDOdisp_$g")
-            end
-            # Dmin_up constraint
-            @constraint(model, [t in dmin_disp[g]:Tmax], UCdisp[t,g] >= sum(UPdisp[i,g] for i in (t-dmin_disp[g]+1):t),  base_name = "dminUPdisp_$g")
-            # Dmin_down constraint
-            @constraint(model, [t in dmin_disp[g]:Tmax], UCdisp[t,g] <= 1 - sum(DOdisp[i,g] for i in (t-dmin_disp[g]+1):t),  base_name = "dminDOdisp_$g")
-            @constraint(model, [t in 1:dmin_disp[g]-1], UCdisp[t,g] >= sum(UPdisp[i,g] for i in 1:t), base_name = "disp_dispUPdisp_$(g)_init")
-            @constraint(model, [t in 1:dmin_disp[g]-1], UCdisp[t,g] <= 1-sum(DOdisp[i,g] for i in 1:t), base_name = "disp_dispDOdisp_$(g)_init")
+#EOD elec
+@constraint(model, [t in 1:Tmax], sum(P_out[a,t] for a in inter) + sum(P_out[a,t] for a in disp) + sum(P_out[a,t] for a in stock) == load[t] + sum(P_in[a,t] for a in stock))
+
+#Availability
+P_out_max_avail = Dict{Tuple{Int,Int}, Float64}()
+P_in_max_avail  = Dict{Tuple{Int,Int}, Float64}()
+
+#To avoid quadratic constraints
+for a in ASSETS, t in 1:Tmax
+    if a in elec_out ∪ gas_out
+        P_out_max_avail[(a,t)] = Pout_max[a] * Avail[a][t]
+    end
+    # if a in elec_in ∪ gas_in
+    #     P_in_max_avail_const[(a,t)] = Pin_max[a] * Avail[a][t]
+    # end
+end
+
+#Pmin/Pmax disp
+@constraint(model, [a in disp, t in 1:Tmax], P_out[a,t] ≤ P_out_max_avail[(a,t)] * on[a,t])
+@constraint(model, [a in disp, t in 1:Tmax], P_out[a,t] ≥ Pout_min[a] * on[a,t])
+#@constraint(model, [a in disp, t in 1:Tmax], P_in[a,t] ≤ P_in_max_avail[(a,t)] * on[a,t])
+#@constraint(model, [a in disp, t in 1:Tmax], P_in[a,t] ≥ Pin_min[a] * on[a,t])
+
+
+#Pmin/Pmax non disp
+#@constraint(model, [a in elec_in ∪ gas_in , t in 1:Tmax], P_in[a,t] ≤ P_in_max_avail[(a,t)])
+@constraint(model, [a in setdiff(elec_out, disp), t in 1:Tmax], P_out[a,t] ≤ P_out_max_avail[(a,t)])
+
+#dmin constraints
+
+@constraint(model, [a in disp, t in 2:Tmax], on[a,t] - on[a,t-1] == up[a,t] - down[a,t])
+
+@constraint(model, [a in disp, t in 1:Tmax], up[a,t] + down[a,t] ≤ 1)
+
+for a in disp
+    if on_init[a] == 0
+        @constraint(model, up[a,1] == on[a,1])
+    else
+        @constraint(model, down[a,1] == 1 - on[a,1])
+    end
+
+    if dmin[a] > 0
+        @constraint(model, [t in dmin[a]:Tmax], on[a,t] ≥ sum(up[a,i] for i in t-dmin[a]+1:t))
+        @constraint(model, [t in dmin[a]:Tmax], on[a,t] ≤ 1 - sum(down[a,i] for i in t-dmin[a]+1:t))
+        @constraint(model, [t in 1:(dmin[a]-1)], on[a,t] >= sum(up[a,i] for i in 1:t))
+        @constraint(model, [t in 1:(dmin[a]-1)], on[a,t] <= 1 - sum(down[a,i] for i in 1:t))
+
     end
 end
 
-#Conversion constraint (il faut avoir les contraintes sur Pin_disp pour décommenter)
-#@constraint(model, [t in 1:Tmax, g in 1:N_disp], Pout_disp[t,g] <= efficacity_disp[g] * Pin_disp[t,g])
+#conv constraints
+@constraint(model, [a in conv_ge, t in 1:Tmax], P_out[a,t] == eff[a] * P_in[a,t])
+
+#Stock constraints
+
+# Initial energy
+E_init = Dict{Int, Float64}()
+
+for a in stock
+    E_init[a] = 0
+end
+
+E_max_avail = Dict{Tuple{Int,Int}, Float64}()
+E_min_avail = Dict{Tuple{Int,Int}, Float64}()
+P_in_max_avail_stock = Dict{Tuple{Int,Int}, Float64}()
+P_out_max_avail_stock = Dict{Tuple{Int,Int}, Float64}()
+
+for a in stock, t in 1:Tmax
+    P_in_max_avail_stock[(a,t)]  = Pout_max[a]
+    P_out_max_avail_stock[(a,t)] = Pout_max[a]
+    E_max_avail[(a,t)] = P_out_max_avail_stock[(a,t)] * duration_t
+    E_min_avail[(a,t)] = 0
+end
+
+@constraint(model, [a in stock], E[a,1] == E_init[a] + (eff[a]*P_in[a,1] - P_out[a,1]) * duration_t)
+
+# Energy stock
+@constraint(model, [a in stock, t in 2:Tmax], E[a,t] == E[a,t-1] + (eff[a]*P_in[a,t] - P_out[a,t]) * duration_t
+)
+
+# Charge boundaries
+@constraint(model, [a in stock, t in 1:Tmax], E[a,t] <= E_max_avail[(a,t)])
+@constraint(model, [a in stock, t in 1:Tmax], E[a,t] >= E_min_avail[(a,t)])
+
+#Discharge init and max
+@constraint(model, [a in stock], P_out[a,1] * duration_t <= E_init[a])
+@constraint(model, [a in stock, t in 2:Tmax], P_out[a,t] * duration_t <= E[a,t-1])
+
+#Charge and discharge available
+@constraint(model, [a in stock, t in 1:Tmax], P_in[a,t] <= P_in_max_avail_stock[(a,t)] * isCharging[a,t])
+@constraint(model, [a in stock, t in 1:Tmax], P_out[a,t] <= P_out_max_avail_stock[(a,t)] * (1 - isCharging[a,t]))
 
 
-#TODO: solve and analyse dispe results
-#solve dispe model
+
+#solve model
 optimize!(model)
 #------------------------------
 #Results
 @show termination_status(model)
 @show objective_value(model)
-
-
-# #exports results as csv file
-# disp_gen = value.(Pdisp)
-
-
-# # new file created
-# touch("results.csv")
-
-# # file handling in write mode
-# f = open("results.csv", "w")
-
-# for name in names
-#     write(f, "$name ;")
-# end
-# write(f, "Hydro ; STEP pompage ; STEP turbinage ; Batterie injection ; Batterie soutirage ; RES ; load ; Net load \n")
-
-# for t in 1:Tmax
-#     for g in 1:Nth
-#         write(f, "$(th_gen[t,g]) ; ")
-#     end
-#     for h in 1:Nhy
-#         write(f, "$(hy_gen[t,h]) ;")
-#     end
-#     write(f, "$(STEP_charge[t]) ; $(STEP_decharge[t]) ;")
-#     write(f, "$(battery_charge[t]) ; $(battery_decharge[t]) ;")
-#     write(f, "$(Pres[t]) ;  $(load[t]) ; $(load[t]-Pres[t]) \n")
-
-# end
-
-# close(f)
-
